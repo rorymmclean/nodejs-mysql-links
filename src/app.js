ca2b0c00a4d0b0408ca6f700841cf62e9220b8e8
rorymmclean <rory.mclean@verizon.net>
2020-06-11 08:24:07 -0400

    add

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.engine('.hbs', exphbs({
   layoutsDir: path.join(app.get('views'), 'layouts'),
   partialsDir: path.join(app.get('views'), 'partials'),
   extname: '.hbs',
-  helpers: require('./lib/handlebars')
+  //helpers: require('./lib/handlebars')
 }))
 app.set('view engine', '.hbs');
 
