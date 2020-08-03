f99ce022fb5648b5a6fa41e77e8f7553c0ddad3a
rorymmclean <rory.mclean@verizon.net>
2020-08-02 23:17:53 -0400

    adding new features

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,11 @@
 const userCtrl = {};
 
+const pool = require("../database");
+
 userCtrl.renderUserProfile = (req, res, next) => {
-  res.render('profile');
+  pool.query('SELECT * FROM openemr.patient_data limit 10', (err, rows) => {
+      res.render('profile', { rows }) 
+  });
 }
 
 module.exports = userCtrl;
