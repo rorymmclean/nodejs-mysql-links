d8669285984a3fc4aa4edd34e78cb56e970e0014
rorymmclean <rory.mclean@verizon.net>
2020-08-03 20:40:23 -0400

    update

--- src/keys.js
+++ src/keys.js
@@ -2,7 +2,7 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: process.env.DATABASE_HOST || 'mysql',
+        host: process.env.DATABASE_HOST || 'localhost',
         user: process.env.DATABASE_USER || 'root',
         password: process.env.DATABASE_PASSWORD || 'root',
         database: process.env.DATABASE_NAME || 'openemr'
